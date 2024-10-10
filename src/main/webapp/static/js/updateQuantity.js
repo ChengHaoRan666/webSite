96561e2f52b80da849d86670be6840c3ce381c77
ChengHaoRan <2640708884@qq.com>
2024-10-10 10:27:33 +0800

    修改购物车页，实现购物车中商品数量改变后商品总价也会改变 TODO：评价页前端设计

--- src/main/webapp/static/js/updateQuantity.js
+++ src/main/webapp/static/js/updateQuantity.js
@@ -1,4 +1,4 @@
-// 修改购物车商品数量
+// 更新商品数量和金额
 function updateQuantity(itemId, action) {
     var quantityInput = document.getElementById('quantity_' + itemId);
     var currentQuantity = parseInt(quantityInput.value);
@@ -12,10 +12,26 @@ function updateQuantity(itemId, action) {
 
     quantityInput.value = newQuantity;
 
+    updateTotalPrice(itemId, newQuantity);
+
     // 发送AJAX请求
     sendUpdateRequest(itemId, newQuantity);
 }
 
+// 更新单个商品的总金额
+function updateTotalPrice(itemId, newQuantity) {
+    // 更新隐藏域中的商品数量，用于多选计算
+    // 选择第一个带有.selectItem类的元素
+    var checkboxElement = document.querySelector('.selectItem');
+    // 更新复选框中的隐藏域中的数据用于更新多选按钮
+    checkboxElement.setAttribute('data-quantity', newQuantity.toString());
+
+
+    var priceElement = document.querySelector(`tr[data-item-id="${itemId}"] .price`);
+    var price = parseFloat(priceElement.textContent);
+    var totalPriceElement = document.querySelector(`tr[data-item-id="${itemId}"] .total-price`);
+    totalPriceElement.textContent = (price * newQuantity).toFixed(2);
+}
 
 // 发送更新数量的AJAX请求
 function sendUpdateRequest(itemId, newQuantity) {
@@ -26,26 +42,33 @@ function sendUpdateRequest(itemId, newQuantity) {
             itemId: itemId,
             quantity: newQuantity
         },
+        success: function (response) {
+            // 更新成功后，确保金额正确
+            updateTotalPrice(itemId, newQuantity);
+        },
+        error: function () {
+            alert('更新数量失败，请重试！');
+        }
     });
 }
 
-// 手动输入时更新购物车商品数量
+// 手动输入时更新购物车商品数量和金额
 function onQuantityChange(itemId) {
     var quantityInput = document.getElementById('quantity_' + itemId);
     var newQuantity = parseInt(quantityInput.value);
-
     if (isNaN(newQuantity) || newQuantity < 1) {
         alert('请输入有效的商品数量！');
         quantityInput.value = 1; // 设置默认值或之前的值
         return;
     }
 
+    updateTotalPrice(itemId, newQuantity);
     sendUpdateRequest(itemId, newQuantity);
 }
 
 // 页面加载完成后，为所有数量输入框添加change事件监听器
-$(document).ready(function() {
-    $('input[type="number"]').on('change', function() {
+$(document).ready(function () {
+    $('input[type="number"]').on('change', function () {
         var itemId = this.id.replace('quantity_', '');
         onQuantityChange(itemId);
     });
