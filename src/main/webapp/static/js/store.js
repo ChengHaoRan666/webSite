7bab4a97daf529c48cb17bc4af715995ba2b90b0
ChengHaoRan <2640708884@qq.com>
2024-10-15 20:51:02 +0800

    实现通过价格排序 TODO：分页，价格区间搜索

--- src/main/webapp/static/js/store.js
+++ src/main/webapp/static/js/store.js
@@ -2,19 +2,13 @@
 document.addEventListener('DOMContentLoaded', function () {
     // 获取价格排序下拉菜单
     var priceSortSelect = document.getElementById('priceSort');
-    console.log(priceSortSelect);
     // 添加事件监听器
     priceSortSelect.addEventListener('change', function () {
         // 获取当前选中的排序方式
         var sortType = this.value;
-        console.log("排序方式：" + sortType);
         // 获取产品列表的容器
         var productList = document.querySelector('.col-md-9 >.row');
         console.log("列表：" + productList);
-        if (!productList) {
-            console.error('产品列表容器不存在');
-            return;
-        }
 
         // 获取所有的产品元素
         var products = Array.from(productList.querySelectorAll('.col-md-4'));
@@ -23,7 +17,6 @@ document.addEventListener('DOMContentLoaded', function () {
             var priceA = parseFloat(a.querySelector('.product-price').textContent.replace('￥', ''));
             var priceB = parseFloat(b.querySelector('.product-price').textContent.replace('￥', ''));
 
-            console.log("价格：" + priceA + " " + priceB);
             if (sortType === '1') { // 从高到低
                 return priceB - priceA;
             } else if (sortType === '2') { // 从低到高
